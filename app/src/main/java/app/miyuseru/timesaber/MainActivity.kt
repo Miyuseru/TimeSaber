f737aa532c7d5e0750fae9188f07ab74af3502b4
miyuu kuwana <miyuu13k@gmail.com>
2020-03-04 14:19:08 +0900

    こみっと

--- app/src/main/java/app/miyuseru/timesaber/MainActivity.kt
+++ app/src/main/java/app/miyuseru/timesaber/MainActivity.kt
@@ -3,9 +3,15 @@ package app.miyuseru.timesaber
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,21 @@ class MainActivity : AppCompatActivity() {
         Button.setOnClickListener{
             startActivity(Intent(applicationContext,CreateActivity::class.java))
         }
+
+
+        // CalendarViewの生成
+        val calendarView = CalendarView(this)
+        calendarView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        // 親レイアウトに、CalendarViewを追加
+        val linearLayout = findViewById<LinearLayout>(R.id.container)
+        linearLayout.addView(calendarView)
+
+
+
     }
-}
+
+
+
+    }
+
---
+++ app/src/main/java/app/miyuseru/timesaber/TodoApplication.kt
@@ -0,0 +1,13 @@
+package app.miyuseru.timesaber
+import android.app.Application
+
+class TodoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
+    }
+}
