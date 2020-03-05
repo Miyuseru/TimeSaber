ed342222cefc6e2d3bc694c4172ef0ebfdb98a8a
miyuu kuwana <miyuu13k@gmail.com>
2020-03-05 16:33:58 +0900

    タスクをれるむに保存する

--- app/src/main/java/app/miyuseru/timesaber/MainActivity.kt
+++ app/src/main/java/app/miyuseru/timesaber/MainActivity.kt
@@ -1,10 +1,8 @@
 package app.miyuseru.timesaber
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CalendarView
-import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,24 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Button.setOnClickListener {
+        button.setOnClickListener {
             startActivity(Intent(applicationContext, CreateActivity::class.java))
         }
 
 
-        // CalendarViewの生成
-        val calendarView = CalendarView(this)
-        calendarView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-
-        // 親レイアウトに、CalendarViewを追加
-        val linearLayout = findViewById<LinearLayout>(R.id.container)
-        linearLayout.addView(calendarView)
-
-
-
     }
 
-
-
-    }
+}
 
